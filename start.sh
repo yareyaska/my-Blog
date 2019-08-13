710a659df99daabdfa4335c5ee55d2df88637820
yareyaska <uraharahussein@gmail.com>
2019-08-13 10:09:38 -0400

    modified start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
-export SECRET_KEY=yu
-export MAIL_USERNAME= 'uraharahussein@gmail.com'
-export MAIL_PASSWORD= 'yussuf5280'
+export SECRET_KEY='yu'
+export MAIL_USERNAME=uraharahussein@gmail.com
+export MAIL_PASSWORD=yussuf5280
 
 export DATABASE_URL= 'postgresql+psycopg2://yusuf:yu123@localhost/blog'
 export QUOTE_API_BASE_URL='http://quotes.stormconsultancy.co.uk/random.json'
