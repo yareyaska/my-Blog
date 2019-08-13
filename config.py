b7d0324579ba8e5dc76faaf7337700375c0a6a6f
yareyaska <uraharahussein@gmail.com>
2019-08-13 10:08:56 -0400

    modified config

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ class Config:
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_USERNAME = ("MAIL_USERNAME")
+    MAIL_PASSWORD = ("MAIL_PASSWORD")
     SUBJECT_PREFIX = 'BLOG'
     SENDER_EMAIL = os.environ.get('MAIL_USERNAME')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
