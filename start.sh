8fe363b78039ed06b055057b723314f2e531d738
yareyaska <uraharahussein@gmail.com>
2019-08-13 08:08:10 -0400

    add start file

---
+++ start.sh
@@ -0,0 +1,8 @@
+export SECRET_KEY=yu
+export MAIL_USERNAME= 'uraharahussein@gmail.com'
+export MAIL_PASSWORD= 'yussuf5280'
+
+export DATABASE_URL= 'postgresql+psycopg2://yusuf:yu123@localhost/blog'
+export QUOTE_API_BASE_URL='http://quotes.stormconsultancy.co.uk/random.json'
+
+python3.6 manage.py server
